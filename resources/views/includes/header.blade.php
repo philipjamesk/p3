fa75b07f7ceecd15b10fe21d974c8dacc7071991
PhilipJames <pjkelnhofer@gmail.com>
2015-10-15 10:28:17 -0400

    Fixed header bar

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,10 +1,24 @@
-<div class="navbar">
-  <a class= "navbar-brand" id="logo" href="/">Lorem Ispum & User Generator</a>
-    <ul class="nav nav-pills">
-      <li><a href="/">Home</a></li>
-      <li><a href="/loremipsum">Lorem Ipsum</a></li>
-      <li><a href="/faker">User Generator</a></li>
-      <li><a href="/logs">More Stuff</a></li>
-    </ul>
-  </div>
-</div>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">PhilipJames.me</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="/">Home</a></li>
+        <li><a href="/loremipsum">Lorem Ipsum</a></li>
+        <li><a href="/faker">User Generator</a></li>
+        <li><a href="/logs">More Stuff</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
