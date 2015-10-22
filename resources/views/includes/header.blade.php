457b187b81dd6678e24cf08f45a8001930b36f62
PhilipJames <pjkelnhofer@gmail.com>
2015-10-22 16:25:23 -0400

    Fixed too many quotes in header classes

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -14,10 +14,10 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
-        <li {{ Request::is('/') ? 'class="active"' :''}}><a href="/">Home</a></li>
-        <li {{ Request::is('lorem') ? 'class="active"' :''}}><a href="/lorem">Lorem Ipsum</a></li>
-        <li {{ Request::is('users') ? 'class="active"' :''}}><a href="/users">User Generator</a></li>
-        <li {{ Request::is('password') ? 'class="active"' :''}}><a href="/password">Password Generator</a></li>
+        <li {{ Request::is('/') ? 'class=active' :''}}><a href="/">Home</a></li>
+        <li {{ Request::is('lorem') ? 'class=active' :''}}><a href="/lorem">Lorem Ipsum</a></li>
+        <li {{ Request::is('users') ? 'class=active' :''}}><a href="/users">User Generator</a></li>
+        <li {{ Request::is('password') ? 'class=active' :''}}><a href="/password">Password Generator</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container -->
